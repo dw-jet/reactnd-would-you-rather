4d5900d09ba2bf9278978cba0ff2970207c262d4
James Teitsworth <james@jamesteitsworth.com>
2020-07-22 22:23:01 -0500

    chore: Set up structure to start building project.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <div className='question'>
+        <p>Hello World.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
