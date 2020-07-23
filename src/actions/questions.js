676d2582c7094ee07187437daa9a3070cd95e34d
James Teitsworth <james@jamesteitsworth.com>
2020-07-23 09:55:23 -0500

    feat: Add dependent actions and helpers.

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,5 +1,18 @@
+import { 
+  saveQuestion, 
+  saveQuestionAnswer 
+} from '../../utils/api'
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
 export const ADD_QUESTION = 'ADD_QUESTION'
+export const ANSWER_QUESTION = 'ANSWER_QUESTION'
+
+export function receiveQuestions(questions) {
+  return {
+    type: RECEIVE_QUESTIONS,
+    questions,
+  }
+}
 
 function addQuestion(question) {
   return {
@@ -8,9 +21,35 @@ function addQuestion(question) {
   }
 }
 
-export function receiveQuestions(questions) {
+export function handleAddQuestion(question) {
+  return (dispatch, getState) => {
+    const { currentUser } = getState()
+
+    return saveQuestion(question)
+      .then((question) => dispatch(addQuestion(question)))
+      .catch((e) => {
+        console.warn('Error in handleAddQuestion: ', e)
+        alert('Error adding question')
+      })
+  }
+}
+
+function answerQuestion({currentUser, qid, answer}) {
   return {
-    type: RECEIVE_QUESTIONS,
-    questions,
+    type: ANSWER_QUESTION,
+    qid,
+    currentUser,
+    answer,
+  }
+}
+
+export function handleAnswerQuestion(data) {
+  return (dispatch) => {
+    dispatch(answerQuestion(data))
+    return saveQuestionAnswer(data)
+      .catch((e) => {
+        console.warn('Error in handleAnswerQuestion: ', e)
+        alert('There was an error saving your answer')
+      })
   }
 }
---
+++ src/actions/shared.js
@@ -0,0 +1,17 @@
+import { receiveUsers } from '../actions/users'
+import { receiveQuestions } from '../actions/questions'
+import { setCurrentUser } from '../actions/currentUser'
+import { handleInitialData } from '../../utils/api'
+
+const DEFAULT_USER = 'tylermcginnis'
+
+export function handleInitialData() {
+  return (dispatch) => {
+    return getInitialData()
+      .then(({ users, questions }) => {
+        dispatch(receiveUsers(users))
+        dispatch(receiveQuestions(questions))
+        dispatch(setCurrentUser(DEFAULT_USER))
+      })
+  }
+}
