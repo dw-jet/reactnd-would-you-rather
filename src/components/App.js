6e372b1d9fffea46b67da5826a285ce4112f9247
James Teitsworth <james@jamesteitsworth.com>
2020-07-24 13:46:06 -0500

    feat: Add styling to make things more readable

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 
   render () {
     return (
-      <div>
+      <div className='container'>
         <h1>Identify yourself</h1>
           <UserPicker />
       </div>
