0247b480983a71764497a4a5897b81fe73d2edb3
James Teitsworth <james@jamesteitsworth.com>
2020-08-06 10:53:00 -0500

    feat: Make polls show results for answered questions.

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -4,19 +4,39 @@ import {formatDate} from '../utils/helpers'
 
 class Poll extends Component {
   render() {
-    const {question} = this.props
+    const {question, answer} = this.props
     return (
-    <p>On {formatDate(question.timestamp)}{question.author} asked ...</p>
+      <div>
+      <p>On {formatDate(question.timestamp)}{question.author} asked would you rather</p>
+      <input type='radio'
+        id='optionOne'
+        name={`${question.id}pollChoice`}
+        value='optionOne'
+        checked={answer === 'optionOne'}
+      />
+      <label htmlFor='optionOne'>{question.optionOne.text}</label>
+      <br />
+      <input type='radio'
+        id='optionTwo'
+        name={`${question.id}pollChoice`}
+        value='optionTwo'
+        checked={answer === 'optionTwo'}
+      />
+      <label htmlFor='optionTwo'>{question.optionTwo.text}</label>
+      </div>
     )
   }
 }
 
 function mapStateToProps ({ users, questions, currentUser }, {id}) {
   const question = questions[id]
+  const user = users[currentUser] ? users[currentUser] : {answers: ''}
+  const answer = user.answers[id] ? user.answers[id] : ''
 
   return {
     currentUser,
-    question: question ? question : null
+    question: question ? question : null,
+    answer,
   }
 }
 
